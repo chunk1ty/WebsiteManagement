e58ecf65e0190b3a2fddf1488ffabb8070b0cf86
chunk1ty <andriyan.krastev@gmail.com>
2021-09-06 19:37:24 +0300

    rename request/response model

--- src/Application/Images/Queries/GetImage/GetImageRequest.cs
+++ src/Application/Images/Queries/GetImage/GetImageRequest.cs
@@ -10,7 +10,7 @@ using WebsiteManagement.Domain;
 
 namespace WebsiteManagement.Application.Images.Queries.GetImage
 {
-    public class GetImageRequest : IRequest<OperationResult<ImageContentOutputModel>>
+    public class GetImageRequest : IRequest<OperationResult<GetImageResponse>>
     {
         public GetImageRequest(Guid websiteId)
         {
@@ -20,7 +20,7 @@ namespace WebsiteManagement.Application.Images.Queries.GetImage
         public Guid WebsiteId { get; }
     }
 
-    public class GetImageHandler : IRequestHandler<GetImageRequest, OperationResult<ImageContentOutputModel>>
+    public class GetImageHandler : IRequestHandler<GetImageRequest, OperationResult<GetImageResponse>>
     {
         private readonly IWebsiteRepository _repository;
 
@@ -29,17 +29,17 @@ namespace WebsiteManagement.Application.Images.Queries.GetImage
             _repository = repository;
         }
 
-        public async Task<OperationResult<ImageContentOutputModel>> Handle(GetImageRequest query, CancellationToken cancellationToken)
+        public async Task<OperationResult<GetImageResponse>> Handle(GetImageRequest query, CancellationToken cancellationToken)
         {
             query = query ?? throw new ArgumentNullException(nameof(query));
 
             Website website = await _repository.GetByIdAsync(query.WebsiteId);
             if (website is null)
             {
-                return OperationResult<ImageContentOutputModel>.Failure(new Dictionary<string, string> { { "WebsiteId", ErrorMessages.WebsiteNotFound } });
+                return OperationResult<GetImageResponse>.Failure(new Dictionary<string, string> { { "WebsiteId", ErrorMessages.WebsiteNotFound } });
             }
 
-            return OperationResult<ImageContentOutputModel>.Success(new ImageContentOutputModel(website.Image.Name, website.Image.MimeType, website.Image.Blob));
+            return OperationResult<GetImageResponse>.Success(new GetImageResponse(website.Image.Name, website.Image.MimeType, website.Image.Blob));
         }
     }
 }
